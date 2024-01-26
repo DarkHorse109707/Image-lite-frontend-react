4121ac620c21d0f7a460097374616c5a57a25070
darkhorse <darkhorse109707@gmail.com>
2024-01-26 15:55:38 -0300

    Padrão Barrels e Criação do componente ImageCard

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,12 +1,15 @@
-import { Template } from '../../components/template'
+import { Template, ImageCard } from '@/components'
 
 export default function GaleriaPage() {
     return (
         <Template>
-
-            <h1>
-                Galeria
-            </h1>
+            <section className="grid grid-cols-4 gap-8">
+                <ImageCard dataUpload='26/01/2024'tamanho='320MB'nome='Imagem 1' src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg' />
+                <ImageCard dataUpload='26/01/2024'tamanho='320MB'nome='Imagem 2' src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg' />
+                <ImageCard dataUpload='26/01/2024'tamanho='320MB'nome='Imagem 3' src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg' />
+                <ImageCard dataUpload='26/01/2024'tamanho='320MB'nome='Imagem 4' src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg' />     
+                <ImageCard dataUpload='26/01/2024'tamanho='320MB'nome='Imagem 4' src='https://s1.static.brasilescola.uol.com.br/be/conteudo/images/imagem-em-lente-convexa.jpg' />     
+            </section>
 
         </Template>
     )
