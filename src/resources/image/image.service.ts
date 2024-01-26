81fb728686c8483bb4b0ebdb3b9ecc8ad0989628
darkhorse <darkhorse109707@gmail.com>
2024-01-26 17:14:43 -0300

    Camada de comunicação com API

---
+++ src/resources/image/image.service.ts
@@ -0,0 +1,14 @@
+import { Image } from './image.resource'
+
+class ImageService {
+    baseUrl: string = 'http://localhost:8080/v1/images';
+
+    async buscar(): Promise<Image[]> {
+        const response = await fetch(this.baseUrl);
+        return await response.json();
+    }
+}
+
+export const useImageService = () => new ImageService(){
+    
+}
