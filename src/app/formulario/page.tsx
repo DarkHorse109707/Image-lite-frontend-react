aa1e6d83e13c34ce41b13be1d1302220ff33866e
darkhorse <darkhorse109707@gmail.com>
2024-01-26 08:55:48 -0300

    Criação Template

---
+++ src/app/formulario/page.tsx
@@ -0,0 +1,7 @@
+export default function FormularioPage(){
+    return (
+        <h1>
+            Formulário
+        </h1>
+    )
+}
---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,13 @@
+import { Template } from '../../components/template'
+
+export default function GaleriaPage() {
+    return (
+        <Template>
+
+            <h1>
+                Galeria
+            </h1>
+
+        </Template>
+    )
+}
